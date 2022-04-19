7e48e08e030c8594bb841a1e3ba8f8f33c8780bd
adung13 <anhdung13022002@gmail.com>
2022-04-19 22:35:41 +0700

    update

---
+++ BaiThucHanh1904/src/App.java
@@ -0,0 +1,26 @@
+import Animal.*;
+import BaiThucHanh.*;
+import BaiThucHanh.NhanVien.Employee;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Person ps = new Person();
+        ps.getName();
+        ps.setName);
+        ps.getAddress();
+        ps.setAddress();
+        ps.getAge();
+        ps.setAge();
+        
+        Student std =new Student();
+        std.getMark();
+        std.setMark);
+        
+        Employee emp = new Employee();
+        emp.xuatThongTin();
+        
+
+        FullTimeEmployee nv1 = new FullTimeEmployee(12);
+        PartTimeEmployee nv2 = new PartTimeEmployee(3);
+}
+}
